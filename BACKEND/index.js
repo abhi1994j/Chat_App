cca3ca5f6321743d95fb5f42d0a4e51cafcd8bed
abhi1994j <abhibaban4@gmail.com>
2025-08-23 13:51:36 +0530

    remove seed

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -21,6 +21,7 @@ connectDB();
 app.use(express.json());
 // app.use(express.json({ limit: "50mb" }));  // allow JSON payload up to 10MB
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 8000;
