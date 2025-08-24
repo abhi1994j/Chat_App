beb6601b81961c88544586f55bf03e921fbcb15f
abhi1994j <abhibaban4@gmail.com>
2025-08-24 23:07:12 +0530

    chat app

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,6 +8,9 @@ import { corsConfig } from "./src/config/cors.js";
 import { authRouter } from "./src/routes/auth.route.js";
 import { messageRouter } from "./src/routes/message.route.js";
 
+import path from "path";
+
+
 const app = express();
 
 app.use(helmet());
@@ -28,6 +31,14 @@ const PORT = process.env.PORT || 8000;
 
 app.use("/api/v1/chat", authRouter);
 app.use("/api/v1/chat", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../FRONTEND/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FRONTEND", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server is running in ${PORT}`);
